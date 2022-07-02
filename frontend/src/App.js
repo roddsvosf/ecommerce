7ffa166b493b936003b17dbd28cb833558b1b8ab
roddsvosf <rodrigo.soares@osf.digital>
2022-07-02 16:38:12 -0300

    First commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Amazona</a>
+      </header>
+      <main>
+        List Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
